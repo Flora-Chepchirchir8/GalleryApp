d3630ca3871ffe9fa800743c5d425318578cad07
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-05-29 21:29:55 +0300

    build files

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,52 +1,90 @@
 from django.test import TestCase
-from .models import Editor,Article,tags
-import datetime as dt
+from .models import Image,Category,Location
 
-# Create your tests here.
-class EditorTestClass(TestCase):
+class ImageTestClass(TestCase):
+    
+    def setUp(self):
 
     # Set up method
-    def setUp(self):
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
-        # Testing  instance
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.james,Editor))
-          # Testing Save Method
-    def test_save_method(self):
-        self.james.save_editor()
-        editors = Editor.objects.all()
-        self.assertTrue(len(editors) > 0)
+        self.new_category = Category(name='Wildlife')
+        self.new_category.save_category()
+            
+        self.new_location = Location(city='Nairobi', country='Kenya')
+        self.new_location.save_location()
+            
+        self.new_picture = Image(img='images/py.jpeg', name='Lion', description='jungle', category=self.new_category, location=self.new_location)
+        self.new_picture.save_image()
         
-
-class ArticleTestClass(TestCase):
-
-    def setUp(self):
-        # Creating a new editor and saving it
-        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
-        self.james.save_editor()
-
-        # Creating a new tag and saving it
-        self.new_tag = tags(name = 'testing')
-        self.new_tag.save()
-
-        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
-        self.new_article.save()
-
-        self.new_article.tags.add(self.new_tag)
-
+       
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_picture,Image))
+        self.assertTrue(isinstance(self.new_category, Category))
+        self.assertTrue(isinstance(self.new_location, Location))   
+          
     def tearDown(self):
-        Editor.objects.all().delete()
-        tags.objects.all().delete()
-        Article.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()   
+    def test_save_image(self):
+        self.assertTrue(len(Image.objects.all()) == 1)
 
-    def test_get_news_today(self):
-        today_news = Article.todays_news()
-        self.assertTrue(len(today_news)>0)
+    def test_delete_image(self):
+        self.new_picture.delete_image()
+        self.assertTrue(len(Image.objects.all()) == 0)
+        
+    # def test_search_image(self):
+    #     '''
+    #     test method to ensure correct searching of an multiple image instances by category
+    #     '''
+    #     self.new_picture = Image.search_by_category(self.new_picture.category)
 
-#........
-    def test_get_news_by_date(self):
-        test_date = '2017-03-17'
-        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
-        news_by_date = Article.days_news(date)
-        self.assertTrue(len(news_by_date) == 0)
+    def test_filter_by_location(self):
+        self.new_picture = Image.filter_by_location(self.new_picture.location)
+        print(self.new_picture)
+    def test_get_image_by_id(self):
+        self.new_picture = Image.get_image_by_id(self.new_picture.id)
+    def test_update_image(self):
+        update_test = self.new_picture.update_image('images/jolly.png')
+        self.assertEqual(update_test.image_link, 'images/jolly.png')
+        
+class CategoryTestClass(TestCase):
+    '''
+    '''
+    def setUp(self):
+        self.new_category = Category(name='Wildlife')
+        self.new_category.save_category()
+    def tearDown(self):
+        Category.objects.all().delete()
+    def test_delete_category(self):
+        self.new_category.save_category()
+        self.new_category.delete_category()
+        self.assertTrue(len(Category.objects.all()) == 0)
+
+    def test_save_category(self):
+        self.assertTrue(len(Category.objects.all()) == 1)   
+   
+    def test_update_category(self):
+        update_test = self.new_category.update_category('Wildlife','Beach')
+        self.assertEqual(update_test.name, 'Beach')
+    
+class LocationTestClass(TestCase):
+    '''
+    test class for Locations model
+    '''
+    def setUp(self):
+        self.new_location = Location(city='Tokyo', country='Japan')
+        self.new_location.save_location()
+
+    def test_save_location(self):
+        self.assertTrue(len(Location.objects.all()) == 1)     
+
+    def test_delete_location(self):
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        self.assertTrue(len(Location.objects.all()) == 0)
+
+    def test_update_location(self):
+        update_location = Location.update_location('Japan', 'Shree')
+        self.assertEqual(update_location.city, 'Shree')
+        
--- pictures/urls.py
+++ pictures/urls.py
@@ -3,11 +3,12 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns =[
-    # re_path(r'^$', views.welcome, name = 'welcome'),
-    re_path(r'^$', views.news_today, name = 'newsToday'),
-    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    re_path(r'^search/', views.search_results, name='search_results')
-]
+urlpatterns=[
+    re_path('^$',views.gallery,name = 'gallery'),
+    re_path(r'^image/',views.display_image,name='displayImages'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^photo/(\d+)',views.image,name ='image'),
+    ] 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
